b6dd8685ce5bb4f162eacfbd034b50d89ec78c11
dude719 <blood_099@hotmail.com>
2016-06-06 23:14:53 -0600

    Add project files.

---
+++ LolUncompress/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Forms;
+using ComponentAce.Compression.Libs.zlib;
+
+namespace LolUncompress
+{
+    public partial class Form1 : Form
+    {
+  
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static void UncompressFile(string inFile, string outFile)
+        {
+            int stopByte = -1;
+            System.IO.FileStream outFileStream = new System.IO.FileStream(outFile, System.IO.FileMode.Create);
+            ZInputStream inZStream = new ZInputStream(System.IO.File.Open(inFile, System.IO.FileMode.Open, System.IO.FileAccess.Read));
+            int data;
+            while (stopByte != (data = inZStream.Read())) {
+                byte _dataByte = (byte)data;
+                outFileStream.WriteByte(_dataByte);
+            }
+            inZStream.Close();
+            outFileStream.Close();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            openFileDialog1.Title = "Select a file to decompress";
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                saveFileDialog1.Title = "Save decompressed file to";
+                saveFileDialog1.FileName = openFileDialog1.FileName + ".decompressed";
+                if (saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    UncompressFile(openFileDialog1.FileName, saveFileDialog1.FileName);
+                }
+            }
+        }
+    }
+}
